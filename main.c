b2c0fca552bb01a54dc60b80301484b1cdb5bb6e
Ismahen5 <ismahen.ensi@gmail.com>
2018-03-05 10:41:47 +0100

    Update main.c

--- main.c
+++ main.c
@@ -8,13 +8,13 @@
 int main(int argc, char* argv[])
 {
     if(argc <= 1) {
-        printf(" g3idaCompiler: fatal error no input files\n");
+        printf(" IsmahenCompiler: fatal error no input files\n");
         return 0;
     }
 
     input = fopen(argv[1], "r");
     if(input == NULL) {
-        printf(" g3idaCompiler: fatal error file not found %s\n", argv[1]);
+        printf(" IsmmahenCompiler: fatal error file not found %s\n", argv[1]);
         return -1;
     }
 
